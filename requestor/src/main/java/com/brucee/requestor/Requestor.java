41dfac7c82070e8d5d9e51e5555235a5f8286816
jun <1010479088@qq.com>
2020-08-14 15:34:55 +0800

    1.requestor模块

---
+++ requestor/src/main/java/com/brucee/requestor/Requestor.java
@@ -0,0 +1,15 @@
+package com.brucee.requestor;
+
+public class Requestor {
+    public static void single() {
+
+    }
+
+    public static void list() {
+
+    }
+
+    public static void multi() {
+
+    }
+}
